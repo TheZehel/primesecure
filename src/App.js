1d9a4b44e54e5574ad15a52de4fb710caa0d7b0a
TheZehel <alls.yuri@gmail.com>
2023-06-15 23:15:38 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Banner from "./components/Banner";
 import NavBarMenu from "./components/navBarMenu.js";
+import Residencial from "./components/Residencial";
 import { Skills } from "./components/Skills";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <NavBarMenu />
       <Banner />
       <Skills />
+      <Residencial />
     </div>
   );
 }
