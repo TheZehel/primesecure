bfbcd9f5b953e294f3d1d5a3d92169eed3535313
Catarina Dalsan Escolar Ribeiro <125068498+Catyz00@users.noreply.github.com>
2025-01-13 16:32:52 -0300

    Update

--- src/components/primetravel/cotador/components/editQuote.js
+++ src/components/primetravel/cotador/components/editQuote.js
@@ -148,8 +148,9 @@ const EditQuote = () => {
   };
 
   return (
-    <div className="w-full bg-white border rounded-lg shadow-md p-4 sm:p-2">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 items-center">
+    <div className="bg-white border rounded-lg shadow-md p-4 sm:p-2 mr-2 ml-2 max-w-[calc(100%-16px)]">
+      {/* Layout para telas maiores */}
+      <div className="hidden lg:grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 items-center">
         <div>
           <h4 className="text-sm font-bold sm:text-xs md:text-sm">Destino</h4>
           {isEditing ? (
@@ -162,11 +163,7 @@ const EditQuote = () => {
                 Selecione o Destino...
               </option>
               {destinations.map((dest) => (
-                <option
-                  key={dest.value}
-                  value={dest.value}
-                  className="text-center"
-                >
+                <option key={dest.value} value={dest.value}>
                   {dest.label}
                 </option>
               ))}
@@ -179,9 +176,7 @@ const EditQuote = () => {
         </div>
 
         <div>
-          <h4 className="text-sm font-bold sm:text-xs md:text-sm">
-            Passageiros
-          </h4>
+          <h4 className="text-sm font-bold sm:text-xs md:text-sm">Passageiros</h4>
           {isEditing ? (
             <button
               onClick={openModal}
@@ -191,63 +186,13 @@ const EditQuote = () => {
             </button>
           ) : (
             <p className="text-base sm:text-sm md:text-base">
-              {formData.olds.reduce((total, age) => total + age, 0)}{' '}
-              Passageiro(s)
+              {formData.olds.reduce((total, age) => total + age, 0)} Passageiro(s)
             </p>
-          )}
-
-          {modalOpen && (
-            <>
-              <div className="fixed inset-0 bg-black bg-opacity-50 z-[1000]" />
-              <div className="fixed inset-0 flex items-center justify-center z-[1001]">
-                <div className="bg-white p-4 rounded-lg shadow-lg max-w-md w-full">
-                  <h2 className="text-xl font-bold mb-4">
-                    Idade dos Passageiros
-                  </h2>
-                  {ageGroups.map((group, index) => (
-                    <div
-                      key={group.id}
-                      className="flex items-center justify-between mb-2"
-                    >
-                      <h3 className="text-xl sm:text-lg md:text-xl">
-                        {group.label}
-                      </h3>
-                      <div className="flex items-center justify-around w-32">
-                        <button
-                          onClick={() => handleOld(index, -1)}
-                          className="bg-bluePrime hover:bg-bluePrime2 text-white w-8 h-8 rounded-full flex items-center justify-center focus:outline-none sm:w-6 sm:h-6 md:w-7 md:h-7"
-                        >
-                          -
-                        </button>
-                        <input
-                          type="text"
-                          value={formData.olds[index]}
-                          readOnly
-                          className="text-center block w-12 h-8 text-lg rounded-lg border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-bluePrime sm:w-10 sm:h-7 sm:text-base md:w-11 md:h-8"
-                        />
-                        <button
-                          onClick={() => handleOld(index, 1)}
-                          className="bg-bluePrime hover:bg-bluePrime2 text-white w-8 h-8 rounded-full flex items-center justify-center focus:outline-none sm:w-6 sm:h-6 md:w-7 md:h-7"
-                        >
-                          +
-                        </button>
-                      </div>
-                    </div>
-                  ))}
-                  <button
-                    onClick={closeModal}
-                    className="bg-bluePrime text-white py-2 px-4 rounded mt-4 w-full"
-                  >
-                    Fechar
-                  </button>
-                </div>
-              </div>
-            </>
           )}
         </div>
 
         <div>
-          <h3 className="text-sm font-bold sm:text-xs md:text-sm">Período</h3>
+          <h4 className="text-sm font-bold sm:text-xs md:text-sm">Período</h4>
           {isEditing ? (
             <div className="grid grid-cols-2 gap-4">
               <div className="w-full">
@@ -277,8 +222,8 @@ const EditQuote = () => {
             <p className="text-base sm:text-sm md:text-base text-center">
               {formData.DataInicioViagem && formData.DataFinalViagem
                 ? `De ${formData.DataInicioViagem.format(
-                    'DD/MM/YYYY',
-                  )} até ${formData.DataFinalViagem.format('DD/MM/YYYY')}`
+                  'DD/MM/YYYY',
+                )} até ${formData.DataFinalViagem.format('DD/MM/YYYY')}`
                 : 'Período não selecionado'}
             </p>
           )}
@@ -298,15 +243,155 @@ const EditQuote = () => {
               onClick={handleEditToggle}
               className="bg-bluePrime text-white py-2 px-4 rounded-md shadow-sm hover:bg-bluePrime2 flex items-center gap-x-2 sm:py-1 sm:px-3 sm:text-sm md:py-2 md:px-4"
             >
-              <Pen
-                className="w-5 h
-5 sm:w-4 sm:h-4 md:w-5 md:h-5"
-              />
-              <span>Editar Cotação</span>
+              <Pen className="w-5 h-5" />
+              <span>Editar</span>
             </button>
           )}
         </div>
       </div>
+
+      {/* Layout para telas menores */}
+      <div className=" lg:hidden text-xs flex flex-col space-y-2">
+        {isEditing ? (
+          <>
+            <div className="flex flex-col space-y-4">
+              <div>
+                <h4 className="text-sm font-bold">Destino</h4>
+                <select
+                  value={formData.selectedOption?.value || ''}
+                  onChange={selectHandler}
+                  className="mt-2 cursor-pointer w-full rounded-md border px-2 py-1 text-sm shadow-sm text-center ring-offset-whitePrime"
+                >
+                  <option value="" disabled>
+                    Selecione o Destino...
+                  </option>
+                  {destinations.map((dest) => (
+                    <option key={dest.value} value={dest.value}>
+                      {dest.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div>
+                <h4 className="text-sm font-bold">Passageiros</h4>
+                <button
+                  onClick={openModal}
+                  className="bg-bluePrime text-white py-1 px-4 rounded shadow hover:bg-bluePrime2"
+                >
+                  Selecionar Passageiros
+                </button>
+              </div>
+
+              <div>
+                <h4 className="text-sm font-bold">Período</h4>
+                <div className="grid grid-cols-2 gap-4">
+                  <div>
+                    <DatePicker
+                      locale={locale}
+                      value={formData.DataInicioViagem}
+                      onChange={onChangeDeparture}
+                      placeholder="Data de ida"
+                      disabledDate={disabledDepartureDate}
+                      format="DD/MM/YYYY"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-bluePrime text-[#313131] placeholder:text-[#313131]"
+                    />
+                  </div>
+                  <div>
+                    <DatePicker
+                      locale={locale}
+                      value={formData.DataFinalViagem}
+                      onChange={onChangeArrival}
+                      placeholder="Volta"
+                      disabledDate={disabledArrivalDate}
+                      format="DD/MM/YYYY"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-bluePrime text-[#313131] placeholder:text-[#313131]"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="flex justify-between items-center">
+            <span>{formData.selectedOption?.label || 'Destino não selecionado'}</span>
+            <span>{formData.olds.reduce((total, age) => total + age, 0)} Passageiro(s)</span>
+            <span>
+              {formData.DataInicioViagem && formData.DataFinalViagem
+                ? `De ${formData.DataInicioViagem.format('DD/MM/YYYY')} até ${formData.DataFinalViagem.format('DD/MM/YYYY')}`
+                : 'Período não selecionado'}
+            </span>
+          </div>
+        )}
+
+        <div className="flex justify-end mt-4">
+          {isEditing ? (
+            <button
+              onClick={handleSave}
+              className="bg-green-500 text-white py-2 px-2 rounded-md shadow-sm hover:bg-green-600 flex items-center gap-x-1 ml-auto"
+            >
+              <Save className="w-3 h-3" />
+              <span>Salvar</span>
+            </button>
+          ) : (
+            <button
+              onClick={handleEditToggle}
+              className="bg-bluePrime text-white py-2 px-2 rounded-md shadow-sm hover:bg-bluePrime2 flex items-center gap-x-1 ml-auto"
+            >
+              <Pen className="w-3 h-3" />
+              <span>Editar</span>
+            </button>
+          )}
+        </div>
+
+      </div>
+
+
+      {/* Modal permanece intacto */}
+      {modalOpen && (
+        <>
+          <div className="fixed inset-0 bg-black bg-opacity-50 z-[1000]" />
+          <div className="fixed inset-0 flex items-center justify-center z-[1001]">
+            <div className="bg-white p-4 rounded-lg shadow-lg max-w-md w-full">
+              <h2 className="text-xl font-bold mb-4">Idade dos Passageiros</h2>
+              {ageGroups.map((group, index) => (
+                <div
+                  key={group.id}
+                  className="flex items-center justify-between mb-2"
+                >
+                  <h3 className="text-xl">{group.label}</h3>
+                  <div className="flex items-center justify-around w-32">
+                    <button
+                      onClick={() => handleOld(index, -1)}
+                      className="bg-bluePrime hover:bg-bluePrime2 text-white w-8 h-8 rounded-full flex items-center justify-center"
+                    >
+                      -
+                    </button>
+                    <input
+                      type="text"
+                      value={formData.olds[index]}
+                      readOnly
+                      className="text-center block w-12 h-8 text-lg rounded-lg border-0 px-3 py-2 text-gray-900 shadow-sm"
+                    />
+                    <button
+                      onClick={() => handleOld(index, 1)}
+                      className="bg-bluePrime hover:bg-bluePrime2 text-white w-8 h-8 rounded-full flex items-center justify-center"
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <button
+                onClick={closeModal}
+                className="bg-bluePrime text-white py-2 px-4 rounded mt-4 w-full"
+              >
+                Fechar
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/primetravel/cotador/components/plans.js
+++ src/components/primetravel/cotador/components/plans.js
@@ -4,6 +4,7 @@ import ModalCoberturas from './modalCoberturas';
 import EditQuote from './editQuote';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { ArrowRight } from 'lucide-react';
 
 const cardData = [
   {
@@ -69,112 +70,123 @@ const Plans = ({ onSelected, setSelectedPlan }) => {
       </div>
       <div>
         <EditQuote />
-        <h2 className="text-3xl font-bold text-[#313131] mt-8">
+        <h2 className="text-base sm:text-2xl lg:text-3xl font-bold text-[#313131] mt-8 mr-8 ml-8">
           Selecionamos os Melhores Planos Para Você.
         </h2>
-        <p className="mb-5">
+
+
+        <p className="text-base sm:text-xl lg:text-2xl mb-5 mr-8 ml-8">
           Agora com prorrogação de estadia disponível em todos os planos!
         </p>
+
+        <p className='text-bluePrime2 font-bold text-base sm:text-xl lg:text-2xl mt-5 mr-8 ml-8'>
+          Arraste para o lado e veja os planos disponíveis
+        </p>
+
+        <ArrowRight className='text-bluePrime2 flex items-center' />
       </div>
 
       {/* Renderizando cards em um Swiper */}
-      <Swiper
-        spaceBetween={20}
-        slidesPerView={1}
-        breakpoints={{
-          640: { slidesPerView: 1 },
-          768: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-        }}
-      >
-        {cardData.map((card) => (
-          <SwiperSlide key={card.id}>
-            <div
-              className={`border rounded-lg shadow-lg p-6 ${selectedPlanId === card.id ? 'border-bluePrime' : ''
-                }`}
-            >
-              <h5 className="text-gray-500 uppercase text-center font-semibold text-sm">
-                {card.title}
-              </h5>
-              <div className="text-center mt-6">
-                <div className="flex flex-col items-center">
-                  <h6 className="text-lg font-medium">
-                    <span className="text-gray-600">12x</span>{' '}
-                    <span className="text-[#313131] text-2xl font-bold">
-                      {card.price}
-                    </span>{' '}
-                    <span className="text-gray-600">Sem Juros</span>
-                  </h6>
-                  <p className="bg-green-500 font-bold text-sm mt-1 text-white py-1 px-2 rounded-md">
-                    {card.discount}
+      <div>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+          className="max-w-full md:max-w-[768px] lg:max-w-[1024px] mx-auto scale-90 md:scale-95 lg:scale-100"
+        >
+          {cardData.map((card) => (
+            <SwiperSlide key={card.id}>
+              <div
+                className={`border rounded-lg shadow-lg p-4 ${selectedPlanId === card.id ? 'border-bluePrime' : ''
+                  }`}
+              >
+                <h5 className="text-gray-500 uppercase text-center font-semibold text-sm md:text-base lg:text-lg">
+                  {card.title}
+                </h5>
+                <div className="text-center mt-4">
+                  <div className="flex flex-col items-center">
+                    <h6 className="text-sm md:text-base lg:text-lg font-medium">
+                      <span className="text-gray-600">12x</span>{' '}
+                      <span className="text-[#313131] text-lg md:text-xl lg:text-2xl font-bold">
+                        {card.price}
+                      </span>{' '}
+                      <span className="text-gray-600">Sem Juros</span>
+                    </h6>
+                    <p className="bg-green-500 font-bold text-xs md:text-sm lg:text-base mt-1 text-white py-1 px-2 rounded-md">
+                      {card.discount}
+                    </p>
+                  </div>
+                  <p className="text-gray-600 mt-2 text-xs md:text-sm lg:text-base">
+                    <span className="font-bold">{card.fullPrice}</span>{' '}
+                    <span className="font-normal">à vista</span>
                   </p>
+                  <div className="border-t border-gray-300 my-4 w-3/4 mx-auto"></div>
+                  <div className="flex justify-between items-center text-xs md:text-sm lg:text-base px-4">
+                    <h6 className="text-gray-700 font-medium">Cobertura Total:</h6>
+                    <h6 className="text-bluePrime font-bold">{card.coverage}</h6>
+                  </div>
                 </div>
-                <p className="text-gray-600 mt-2 text-sm">
-                  <span className="font-bold">{card.fullPrice}</span>{' '}
-                  <span className="font-normal">à vista</span>
-                </p>
-                <div className="border-t border-gray-300 my-4 w-3/4 mx-auto"></div>
-                <div className="flex justify-between items-center text-sm px-4">
-                  <h6 className="text-gray-700 font-medium">
-                    Cobertura Total:
-                  </h6>
-                  <h6 className="text-bluePrime font-bold">{card.coverage}</h6>
-                </div>
-              </div>
-              <div className="flex flex-col items-center mt-6 space-y-3">
-                <button
-                  onClick={() => handleCardSelected(card.id)}
-                  className={`${selectedPlanId === card.id
+                <div className="flex flex-col items-center mt-6 space-y-3">
+                  <button
+                    onClick={() => handleCardSelected(card.id)}
+                    className={`${selectedPlanId === card.id
                       ? 'bg-bluePrime2'
                       : 'bg-bluePrime'
-                    } cursor-pointer text-white uppercase text-sm py-2 px-4 rounded-md shadow-md w-full flex items-center justify-center`}
-                >
-                  <input
-                    className="accent-bluePrime rounded-full mr-2 cursor-pointer"
-                    type="checkbox"
-                    checked={selectedPlanId === card.id}
-                    onChange={(e) => {
-                      e.stopPropagation();
-                      handleCardSelected(card.id);
-                    }}
-                  />
-                  {selectedPlanId === card.id ? 'Selecionado' : 'Contratar'}
-                </button>
+                      } cursor-pointer text-white uppercase text-xs md:text-sm lg:text-base py-2 px-4 rounded-md shadow-md w-full flex items-center justify-center`}
+                  >
+                    <input
+                      className="accent-bluePrime rounded-full mr-2 cursor-pointer"
+                      type="checkbox"
+                      checked={selectedPlanId === card.id}
+                      onChange={(e) => {
+                        e.stopPropagation();
+                        handleCardSelected(card.id);
+                      }}
+                    />
+                    {selectedPlanId === card.id ? 'Selecionado' : 'Contratar'}
+                  </button>
+                </div>
+                <hr className="my-4" />
+                <ul className="space-y-2 text-start">
+                  <li className="flex items-center">
+                    <span className="text-green-500 mr-2">✔</span>
+                    Despesas Médicas e Hospitalares (incluso Covid-19)
+                  </li>
+                  <li className="flex items-center">
+                    <span className="text-green-500 mr-2">✔</span>
+                    Despesas odontológicas
+                  </li>
+                  <li className="flex items-center">
+                    <span className="text-green-500 mr-2">✔</span>
+                    Despesas farmacêuticas
+                  </li>
+                  <li className="flex items-center">
+                    <span className="text-green-500 mr-2">✔</span>
+                    Perda de bagagem
+                  </li>
+                  <li className="flex items-center">
+                    <span className="text-green-500 mr-2">✔</span>
+                    Atraso de bagagem (superior a 8h)
+                  </li>
+                  <li className="flex items-center">
+                    <span className="text-green-500 mr-2">✔</span>
+                    Cancelamento ou Atraso de voo (superior a 8h)
+                  </li>
+                  <li className="flex items-center">
+                    <span className="text-green-500 mr-2">✔</span>
+                    E muito mais...
+                  </li>
+                </ul>
               </div>
-              <hr className="my-4" />
-              <ul className="space-y-2 text-start">
-                <li className="flex items-center">
-                  <span className="text-green-500 mr-2">✔</span>
-                  Despesas Médicas e Hospitalares (incluso Covid-19)
-                </li>
-                <li className="flex items-center">
-                  <span className="text-green-500 mr-2">✔</span>
-                  Despesas odontológicas
-                </li>
-                <li className="flex items-center">
-                  <span className="text-green-500 mr-2">✔</span>
-                  Despesas farmacêuticas
-                </li>
-                <li className="flex items-center">
-                  <span className="text-green-500 mr-2">✔</span>
-                  Perda de bagagem
-                </li>
-                <li className="flex items-center">
-                  <span className="text-green-500 mr-2">✔</span>
-                  Atraso de bagagem (superior a 8h)
-                </li>
-                <li className="flex items-center">
-                  <span className="text-green-500 mr-2">✔</span>
-                  Cancelamento ou Atraso de voo (superior a 8h)
-                </li>
-                <li className="flex items-center">
-                  <span className="text-green-500 mr-2">✔</span>E muito mais...
-                </li>
-              </ul>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
 
       <div className="flex flex-col items-center mt-6 space-y-3">
         <ModalCoberturas />
