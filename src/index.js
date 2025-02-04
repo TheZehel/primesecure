1e6baff0518b769409fb4b90f754017fa38b5633
Allison Yuri <40338591+TheZehel@users.noreply.github.com>
2025-02-04 14:14:00 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -5,22 +5,29 @@
  * @module index
  */
 
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
+
+const theme = createTheme({
+  // suas configurações de tema aqui
+});
 
 /**
  * Renderiza o componente App na árvore de DOM.
  */
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
 
 /**
