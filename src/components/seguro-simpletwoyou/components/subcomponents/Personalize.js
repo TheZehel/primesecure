ac3044aa88a3c99f59c8b0f1cc4b2c32c5f09ade
Catarina Dalsan Escolar Ribeiro <125068498+Catyz00@users.noreply.github.com>
2025-03-05 16:28:56 -0300

    update
    troquei a imagem

--- src/components/seguro-simpletwoyou/components/subcomponents/Personalize.js
+++ src/components/seguro-simpletwoyou/components/subcomponents/Personalize.js
@@ -5,8 +5,8 @@ export default function PersonalizarContainer() {
     <div className="flex flex-col md:flex-row items-center md:items-start justify-center gap-8 md:gap-44 p-4 mt-28">
       <div>
         <img
-          src="https://storage.googleapis.com/primesecure/lp-celular/personalize.png"
-          className="border-2 border-white rounded-md shadow-lg"
+          src="https://storage.googleapis.com/primesecure/lp-celular/Seguros.png"
+          className="w-64 border-2 border-white rounded-md shadow-lg"
           alt="Coberturas Personalização"
         />
       </div>
